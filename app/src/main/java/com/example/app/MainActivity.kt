7591ef4147f11f640c541b832e14fa4bd61e8995
Ricardo Santil <ricardosantil18@hotmail.com>
2020-10-20 19:52:50 +0100

    Class for holding data 2

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,42 +1,31 @@
 package com.example.app
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.example.app.dataclasses.Tema
+import android.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var myList: ArrayList<Tema>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        myList = ArrayList<Tema>()
+
+        for (i in 0 until 5){
+            myList.add(Tema(titulo = "Titanic", subtitulo = "Drama", nome = "Dicaprio"))
         }
-    }
+        recycler_view.adapter = LineAdapter(myList)
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
 
