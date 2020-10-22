e991e4a256a846f72d1a92f5186746bba539eeda
Ricardo Santil <ricardosantil18@hotmail.com>
2020-10-22 10:39:26 +0100

    Class for holding data 2

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.example.app.dataclasses.Tema
-import android.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +22,12 @@ class MainActivity : AppCompatActivity() {
 
         myList = ArrayList<Tema>()
 
-        for (i in 0 until 5){
+        for (i in 0 until 5)
+        {
             myList.add(Tema(titulo = "Titanic", subtitulo = "Drama", nome = "Dicaprio"))
         }
-        recycler_view.adapter = LineAdapter(myList)
-
-
+       // recycler_view.adapter = LineAdapter(myList)
+        //recycler_view.layoutManager = LinearLayoutManager (this)
     }
 }
 
